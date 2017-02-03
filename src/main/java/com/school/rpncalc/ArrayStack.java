191e84194b7d19219f75cb8c56838f565b1cbf49
Kasper Vetter <Kasper Vetter>
2017-02-03 22:39:25 +0100

    Finished assignment for calculator

---
+++ src/main/java/com/school/rpncalc/ArrayStack.java
@@ -0,0 +1,76 @@
+package com.school.rpncalc;
+
+import java.util.Iterator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author kaspe
+ */
+public class ArrayStack<T> implements Iterable<T> {
+
+    private T[] data = null;
+    private int top = 0;
+
+    public ArrayStack() {
+        data = (T[]) new Object[1];
+    }
+
+    public void push(T element) {
+
+        if (data.length == top) {
+            T[] dataTemp = data;
+            data = (T[]) new Object[data.length * 2];
+            for (int i = 0; i < dataTemp.length; i++) {
+                data[i] = dataTemp[i];
+            }
+        }
+        data[top] = element;
+        top++;
+
+    }
+
+    public int length() {
+        return data.length;
+    }
+
+    public T pop() {
+        top--;
+        T result = data[top];
+        data[top] = null;
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return top == 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<T> it = new Iterator<T>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < data.length && data[currentIndex] != null;
+            }
+
+            @Override
+            public T next() {
+                return data[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
+
+}
---
+++ src/main/java/com/school/rpncalc/main/Main.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.school.rpncalc.main;
+
+import com.school.rpncalc.ArrayStack;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kaspe
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a number: ");
+        // creating instances of my stack to hold the numbers and math symbols
+        ArrayStack mathToDO = new ArrayStack();
+        ArrayStack stack = new ArrayStack();
+        
+        // setting up while loop to collect user input
+        while (true) {
+            
+            // collecting user input, and splitting each number/symbol seperated by a space
+            String input = reader.nextLine();
+            String[] inputArray = input.split(" ");
+
+            
+            //looping over the input to see which is a number and which is a symbol
+            // and saving it in the correct stack
+            for (String string : inputArray) {
+
+                try {
+                    stack.push(Integer.parseInt(string));
+
+                } catch (NumberFormatException e) {
+                    mathToDO.push(string);
+                }
+            }
+
+            //checking to see if there are any calculations that needs to be done
+            while (!mathToDO.isEmpty()) {
+                // we need atleast 2 numbers in the stack to perform a calculation
+                if (stack.length() > 1) {
+                    switch ((String) mathToDO.pop()) {
+                        case "+":
+                            stack.push((int) stack.pop() + (int) stack.pop());
+                            break;
+                        case "*":
+                            stack.push((int) stack.pop() * (int) stack.pop());
+                            break;
+                        case "/":
+                            stack.push((int) stack.pop() / (int) stack.pop());
+                            break;
+                        case "-":
+                            stack.push((int) stack.pop() - (int) stack.pop());
+                            break;
+                    }
+                } else {
+                    System.out.println("Too few numbers to calculate");
+                }
+            }
+            System.out.println("Numbers in stack: ");
+            for (Object object : stack) {
+                System.out.println(object.toString());
+            }
+
+            if (input.equals("exit")) {
+                break;
+            }
+        }
+    }
+
+}
